295d1e6ac15efa7f9e41ce090ab91ef1b8034aed
Rustam Sharipov <rustamskysan@gmail.com>
2023-12-22 09:07:02 +0500

    s

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, event
 
-# DATABASE_URL = 'postgresql://default:X6CaL40fHeFHsmiAzhqtJuXKf930wnzf@dpg-cm18f9mn7f5s73e5avt0-a.oregon-postgres.render.com/homework_o2iz'
-DATABASE_URL = 'postgresql://postgres:root@localhost:5432/homework'
+DATABASE_URL = 'postgresql://default:X6CaL40fHeFHsmiAzhqtJuXKf930wnzf@dpg-cm18f9mn7f5s73e5avt0-a.oregon-postgres.render.com/homework_o2iz'
+# DATABASE_URL = 'postgresql://postgres:root@localhost:5432/homework'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
